3dfb5a425049cd6714befb11825761ab864cd11b
Sebastian Janzen <sebastian@janzen.it>
2017-12-13 16:12:22 +0100

    MRC-1771 extract react components - Accordion

---
+++ Accordion/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './index.scss';
+import ReactCollapsible from 'react-collapsible';
+import ArrowRight from '../../icons/arrow-right-12.svg';
+
+export class Accordion extends Component {
+
+    render() {
+        return <div className='mrc-accordion' {...this.props}/>;
+    }
+}
+
+export class Collapsible extends Component {
+
+    render() {
+        const trigger = (
+            <span className='trigger-content'>
+                <h1>{this.props.trigger}</h1>
+                <img src={ArrowRight}/>
+            </span>);
+        return <ReactCollapsible transitionTime={200} {...this.props} trigger={trigger}/>;
+    }
+}
