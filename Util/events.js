5a3ea0ffb44544600b3897439490c7ee63b293ae
Sebastian Janzen <sebastian@janzen.it>
2017-12-15 18:06:51 +0100

    MRC-1732 MRC-1771 extract and IE11 compat

--- Util/events.js
+++ Util/events.js
@@ -2,3 +2,25 @@ export const navFlyoutVisibilityChangeEvent = (visibilityState) => ({
     type: 'NAV_FLYOUT_VISIBILITY_CHANGE',
     visibilityState
 });
+
+export const resizeEvent = (tablet, desktop) => ({
+    type: 'RESIZE',
+    tablet,
+    desktop
+});
+
+export const showError = (error, showReloadBtn = false) => ({
+    type: 'ERROR',
+    error,
+    showReloadBtn
+});
+
+export const showWarning = (message) => ({
+    type: 'WARNING',
+    message
+});
+
+export const hideError = () => ({
+    type: 'ERROR',
+    error: null
+});
