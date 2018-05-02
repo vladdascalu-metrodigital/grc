9c69af054d7284531ebe11f64469075bdaea3b41
joergplewe <joerg.plewe@innoq.com>
2018-05-02 17:32:17 +0200

    MRC-2250 add a private helper to resolve template expressions

--- j18n/index.js
+++ j18n/index.js
@@ -2,9 +2,31 @@ import loader from './loader';
 
 let _dataFn = () => loader.translations;
 
-let t = (key) => {
+//
+// helper: resolve template values in translation string
+//
+let _replace = (translation, replacements) => {
+    if (typeof translation === 'string') {
+        let result = translation;
+        Object.keys(replacements).forEach((replacement) => {
+            result = result.split(`%{${replacement}}`).join(replacements[replacement]);
+        });
+        return result;
+    }
+    if (typeof translation === 'object') {
+        const result = {};
+        Object.keys(translation).forEach((translationKey) => {
+            result[translationKey] = this._replace(translation[translationKey], replacements);
+        });
+        return result;
+    }
+    return null;
+};
+
+
+let t = (key, replacement = {} ) => {
     if (!_dataFn || !_dataFn() || !_dataFn()[key]) return key;
-    return _dataFn()[key];
+    return _replace(_dataFn()[key], replacement);
 };
 
 t.setDataFn = (fn) => { _dataFn = fn; };
