29879c8f43073d03ea20aee736dd17f0ea66a309
Stefan Zaharia <stefan.zaharia@metrosystems.net>
2018-07-12 15:33:59 +0300

    MRC-2373 - Changes in Attachments assets and files. + add icons.

--- Attachments/AttachmentsRows.js
+++ Attachments/AttachmentsRows.js
@@ -1,42 +1,155 @@
 import React, {Component} from 'react';
 import './AttachmentsRows.scss';
+import './attachments.scss';
 import PropTypes from 'prop-types';
 import {lookup} from '../Util/translations.js';
+import FileUpload from '../FileUpload';
+//icons:
+import DocIcon from '../icons/doc.svg';
+import PdfIcon from '../icons/pdf.svg';
+import CsvIcon from '../icons/csv.svg';
+import XlsIcon from '../icons/xls.svg';
+import TifIcon from '../icons/tif.svg';
+import PngIcon from '../icons/png.svg';
+import JpgIcon from '../icons/jpg.svg';
+import UnknownIcon from '../icons/doc.svg'; //TODO : need to replace this with an 'unknown' icon. This is just for development phase
+
 
 export default class AttachmentsRows extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {title: '', file: null};
+        
+    }
+
     render() {
         if (!(this.props.data && this.props.data.length > 0)) {
-            return <span>{lookup('mrc.attachments.noattachments')}</span>;
+            {return this.createNoAttachmentsView();}
         }
-        return <ul className='mrc-attachments-rows'>{this.props.data.map(this.createRow)}</ul>;
+        return <div className="mrc-attachments">
+                <div className="tiles-mobile">{this.props.data.map(this.createRow)}
+                 {this.createUploader()}
+                 </div>
+                </div>;
+    }
+
+
+    createNoAttachmentsView(){
+        return <div className="mrc-attachments">
+                <div className="tiles-mobile"><ul><span>{lookup('mrc.attachments.noattachments')}</span></ul>
+                 {this.createUploader()}
+                 </div>
+                </div>;
+    }
+
+    createUploader() {
+        const readyToSend = this.state.title.trim().length > 0 && this.state.file !== null;
+        return <ul>
+                        <label>{lookup('mrc.attachments.fields.title')}</label>
+                        <div className='mrc-input'>
+                            <input name='title' type='text' value={this.state.title} onChange={this.updateTitle}
+                            disabled={this.props.readonly} maxLength={255}/>
+                        </div>
+                        <label name='selected-file' className='selected-file'>{lookup('mrc.attachments.fields.file')}: {this.state.file && this.state.file.name}</label>
+                        <FileUpload labelSelect={lookup('mrc.file.select')}
+                                labelUpload={lookup('mrc.file.upload')}
+                                updateFile={this.updateFile}
+                                sendFile={this.sendFile}
+                                selectDisabled={this.props.readonly}
+                                uploadDisabled={!readyToSend}/>
+                    </ul>;
     }
 
     createRow = (item) => {
         if (!item) {
             return null;
         }
-        return <li key={item.id}>
-            <div className='mrc-attachments-rows-info'>
-                <div className='mrc-attachments-rows-container'>
-                    {item.uploadTimestamp ? this.createTs(item.uploadTimestamp) : null}
-                    <span>{item.uploaderPosition}</span>
-                </div>
-                <div className='mrc-attachments-rows-user'>
-                    {item.uploaderPrincipalName}
-                </div>
+        return <ul key={item.id}>
+            <div className="mrc-square">
+            <div className="square-content" onClick={this.downloadFile.bind(this,item)}>
+                <h3 className="span-metro-blue">{item.title}</h3>
+                <mrc-datetime class="datetime"><b>{item.uploadTimestamp}</b></mrc-datetime>
+                <span className="author"> <b>{item.uploaderPrincipalName} ({item.uploaderPosition})</b></span>
             </div>
-            <a target='_blank' href={item.contentUri}>{item.title}</a>
-        </li>;
+            <div className="edge-line"></div>
+            {this.displayIcon(item)}
+        </div>
+        </ul>;
     };
+
+    displayIcon(item){
+        return <div className="icon-wrapper">
+                    <img className="m-icon-large" src={this.getIcon(item).src} alt={this.getIcon(item).extension + ' File'}></img>
+                </div>;   
+    }
+
+    downloadFile(item){
+        window.open(
+          item.contentUri,
+          '_blank'
+        );
+    }
+
+    getIcon(item) {
+        const re = /(?:\.([^.]+))?$/;
+        const extension = re.exec(item.filename)[1];
+        if(extension == 'doc' || extension == 'docx'){
+            return {src:DocIcon, extension : extension};
+        }
+        else if(extension == 'pdf'){
+            return {src:PdfIcon, extension : extension};
+        }
+        else if(extension == 'csv'){
+            return {src:CsvIcon, extension : extension};
+        }
+        else if(extension == 'xls' || extension == 'xlsx'){
+            return {src:XlsIcon, extension : extension};
+        }
+        else if(extension == 'tif'){
+            return {src:TifIcon, extension : extension};
+        }
+        else if(extension == 'png'){
+            return {src:PngIcon, extension : extension};
+        }
+        else if(extension == 'jpg'){
+            return {src:JpgIcon, extension : extension};
+        }
+        else{ //We need an icon for unknown file types.
+            return {src:UnknownIcon, extension: extension};
+        }
+
+    }
 
     createTs(ts) {
         return <div className='registration-date'>
             <mrc-datetime>{ts}</mrc-datetime>
         </div>;
     }
+
+    updateFile = (file) => {
+        if (this.state.title.trim().length > 0) {
+            this.setState({file: file});
+        } else {
+            this.setState({title: file.name, file: file});
+        }
+    };
+
+    sendFile = () => {
+        this.props.addAttachment(this.state.file, this.state.title);
+        this.setState({title: '', file: null});
+    };
+
+    updateTitle = (evt) => {
+        evt.preventDefault();
+        this.setState({title: evt.target.value});
+    };
+    
+
 }
 
 AttachmentsRows.propTypes = {
-    data: PropTypes.array
+    addAttachment: PropTypes.func.isRequired,
+    data: PropTypes.array,
+    readonly : PropTypes.bool,
 };
--- Attachments/index.js
+++ Attachments/index.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import './index.scss';
 import Panel from '../Panel';
-import PanelItem from '../Panel/PanelItem';
-import FileUpload from '../FileUpload';
 import AttachmentsRows from './AttachmentsRows';
 import PropTypes from 'prop-types';
 import {lookup} from '../Util/translations.js';
@@ -15,45 +13,13 @@ export default class Attachments extends Component {
     }
 
     render() {
-        const readyToSend = this.state.title.trim().length > 0 && this.state.file !== null;
-        return <Panel title={lookup('mrc.attachments.title')} className='mrc-attachments'>
-            <PanelItem>
-                <AttachmentsRows data={this.props.data}/>
-            </PanelItem>
-            <PanelItem>
-                <label>{lookup('mrc.attachments.fields.title')}</label>
-                <div className='mrc-input'>
-                    <input name='title' type='text' value={this.state.title} onChange={this.updateTitle}
-                           disabled={this.props.readonly} maxLength={255}/>
-                </div>
-                <label name='selected-file' className='selected-file'>{lookup('mrc.attachments.fields.file')}: {this.state.file && this.state.file.name}</label>
-                <FileUpload labelSelect={lookup('mrc.file.select')}
-                            labelUpload={lookup('mrc.file.upload')}
-                            updateFile={this.updateFile}
-                            sendFile={this.sendFile}
-                            selectDisabled={this.props.readonly}
-                            uploadDisabled={!readyToSend}/>
-            </PanelItem>
-        </Panel>;
+        return <Panel title={lookup('mrc.attachments.title')}>
+                    <AttachmentsRows data={this.props.data}
+                    readOnly = {this.props.readonly} addAttachment = {this.props.addAttachment} />
+               </Panel>;
+
     }
 
-    updateFile = (file) => {
-        if (this.state.title.trim().length > 0) {
-            this.setState({file: file});
-        } else {
-            this.setState({title: file.name, file: file});
-        }
-    };
-
-    sendFile = () => {
-        this.props.addAttachment(this.state.file, this.state.title);
-        this.setState({title: '', file: null});
-    };
-
-    updateTitle = (evt) => {
-        evt.preventDefault();
-        this.setState({title: evt.target.value});
-    };
 }
 
 Attachments.propTypes = {
