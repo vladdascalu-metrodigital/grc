5a3a86749a872eab260d9ab60b423f98155e07e9
Sonja Heinen <sonja.heinen@gmail.com>
2019-03-15 15:52:28 +0100

    MRC-2ß54 | Fix Button componente.

--- Button/index.js
+++ Button/index.js
@@ -3,19 +3,21 @@ import classNames from 'classnames';
 import './index.scss';
 
 export default class Button extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+    }
 
-        const species = this.props.species
-        const classnames = classNames(
+    render() {
+        const species = this.props.species;
+        const buttonClasses = classNames(
             'mrc-btn',
             {'mrc-primary-button': species === 'primary'},
             {'mrc-secondary-button': species === 'secondary'},
             {'mrc-error-button': species === 'error'},
             {'mrc-success-button': species === 'success'},
-        )
-
+        );
         return (
-            <button className={classnames} type={props.type || 'button'} onClick={props.onClick} disabled={props.disabled}>
+            <button className={buttonClasses} type={props.type || 'button'} onClick={props.onClick} disabled={props.disabled}>
                 {props.icon ? <img className='icon' src={props.icon}/> : null}
                 {props.text}
             </button>
