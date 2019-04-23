0a5ca9162228f1a27168125af14f2d83a1320ce6
Goran Vukovic <goran.vukovic@innoq.com>
2019-04-23 15:24:07 +0200

    MRC-3135 added button and file upload as example components as a beginning

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,16 +4,19 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+import Button from '../Button';
+import FileUpload from '../FileUpload';
+
+import '../node_modules/mrc-component-library/public/css/bundle.css';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+  .add('Primary', () => <Button status="primary" onClick={action('clicked')} text="Approve" />)
+  .add('Error', () => <Button status="error" onClick={action('clicked')} text="Approve" />)
+  .add('Secondary', () => <Button status="secondary" onClick={action('clicked')} text="Approve" />)
+  .add('Success', () => <Button status="success" onClick={action('clicked')} text="Approve" />);
+
+storiesOf('FileUpload', module)
+  .add('standard', () => <FileUpload labelSelect={"select file"} updateFile={action('uploadAttachment')} selectDisabled={false} uploadDisabled={false}/>);
