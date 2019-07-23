f163ad4e50bc5363729eadf1b470bb0bffd457ac
Fei Qin <frei.qin@metronom.com>
2019-07-23 11:42:30 +0200

    MRC-3205: added datepicker for ui

---
+++ DatePicker/index.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import './index.scss';
+import PropTypes from 'prop-types';
+
+export default class MrcDatePickerInput extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {selectedDate: props.selected ? props.selectedDate : null};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+        this.props.onChange(date);
+    }
+
+    render() {
+        const inputProps = {...this.props};
+        delete inputProps.onChange;
+        return (
+            <DatePicker onChange={this.handleChange}
+                    {...this.props} />
+        );
+    }
+
+}
+
+MrcDatePickerInput.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    onBlur: PropTypes.func,
+    className: PropTypes.string,
+    selected: PropTypes.object,
+    disabled: PropTypes.bool,
+    minDate: PropTypes.object,
+    showYearDropdown: PropTypes.bool,
+    dateFormat: PropTypes.string,
+    placeholderText: PropTypes.string
+};
