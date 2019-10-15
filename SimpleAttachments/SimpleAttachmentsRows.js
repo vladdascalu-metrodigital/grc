db0baec5ecaade06747b015445f0b3e251331e86
Fei Qin <frei.qin@metronom.com>
2019-10-15 12:46:08 +0200

    changed file name

--- SimpleAttachments/SimpleAttachmentsRows.js
+++ SimpleAttachments/SimpleAttachmentsRows.js
@@ -14,7 +14,7 @@ import PngIcon from '../icons/png.svg';
 import JpgIcon from '../icons/jpg.svg';
 import UnknownIcon from '../icons/doc.svg'; //TODO : need to replace this with an general 'unknown' icon.
 
-export default class AttachmentsRows extends Component {
+export default class SimpleAttachmentsRows extends Component {
 
 
     constructor(props) {
