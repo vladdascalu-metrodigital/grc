31fe8846d8352d5e7a50890d03833b5d605d1d44
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2020-01-30 15:23:49 +0100

    Revert "Add selected class if sidebar item is highlighted"
    This reverts commit 56bd62a33c231d09343459c8d27405e0b327d90a.

--- Navigation/SidebarNavigation.js
+++ Navigation/SidebarNavigation.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './sidebar.scss';
 import PropTypes from 'prop-types';
-import { createNav, extractNavsFromQuickNav } from '../Util/nav';
+import {createNav, extractNavsFromQuickNav} from '../Util/nav';
 
 export default class SidebarNavigation extends Component {
-    createNavWithWrapper = btnConf => {
-        return (
-            <li className={btnConf.highlight ? 'selected' : ''} key={btnConf.roleKey}>
-                {this.createNavElement(btnConf)}
-            </li>
-        );
+
+    createNavWithWrapper = (btnConf) => {
+        return <li key={btnConf.roleKey}>{this.createNavElement(btnConf)}</li>;
     };
 
     createNavElement(btnConf) {
         const conf = createNav(btnConf, this.props.config.data.translations);
-        if (conf.isAbsolute) {
+        if(conf.isAbsolute) {
             return <a href={conf.href}>{conf.imgEl}</a>;
         } else {
             return <Link to={conf.href}>{conf.imgEl}</Link>;
@@ -24,13 +21,16 @@ export default class SidebarNavigation extends Component {
 
     render() {
         return (
-            <div className="primary-actions">
-                <ul>{extractNavsFromQuickNav(this.props.config.data.quickNav).map(this.createNavWithWrapper, this)}</ul>
+            <div className='primary-actions'>
+                <ul>
+                    {extractNavsFromQuickNav(this.props.config.data.quickNav).map(this.createNavWithWrapper, this)}
+                </ul>
             </div>
         );
     }
+
 }
 
 SidebarNavigation.propTypes = {
-    config: PropTypes.object.isRequired,
+    config: PropTypes.object.isRequired
 };
