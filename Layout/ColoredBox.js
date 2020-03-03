abac7a88b91847164a144111d8af8d64030e4dd8
Sundstroem Ludvig <ludvig.sundstroem@metronom-external.com>
2020-03-03 13:45:08 +0100

    Rename emphasis size for boxes

--- Layout/ColoredBox.js
+++ Layout/ColoredBox.js
@@ -4,33 +4,28 @@ import classnames from 'classnames';
 
 import './ColoredBox.scss';
 
-const ColoredBox = ({children, color, emphasis}) => {
-        const classes = classnames('mrc-colored-box', color, emphasis);
-        return (
-            <div className={classes}>{children}</div>
-        );
+const ColoredBox = ({ children, color, size }) => {
+    const classes = classnames('mrc-colored-box', color, size);
+    return <div className={classes}>{children}</div>;
 };
 
 const COLORS = {
-    'GREEN': 'mrc-green',
-    'GREY_DARK': 'mrc-grey-dark',
-    'GREY_LIGHT': 'mrc-grey-light',
+    GREEN: 'mrc-green',
+    GREY_DARK: 'mrc-grey-dark',
+    GREY_LIGHT: 'mrc-grey-light',
 };
 
-const EMPHASIS = {
-    'PRIMARY': 'cb-primary',
-    'SECONDARY': 'cb-secondary',
-    'TERTIARY': 'cb-tertiary',
+const SIZES = {
+    L: 'cb-large',
+    M: 'cb-medium',
+    S: 'cb-small',
 };
 
 ColoredBox.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.node,
-        PropTypes.arrayOf(PropTypes.node)
-    ]),
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
     color: PropTypes.oneOfType(COLORS),
-    emphasis: PropTypes.oneOfType(EMPHASIS),
+    size: PropTypes.oneOfType(SIZES),
 };
 
 export default ColoredBox;
-export { COLORS, EMPHASIS };
+export { COLORS, SIZES };
