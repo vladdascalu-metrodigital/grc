a86aac77f5d2b98147520d594dfae94b6eb1d742
MRC-NodeRED <ludvig.sundstroem@innoq.com>
2020-04-01 15:23:21 +0200

    Add contracting type for poland

--- Attachment/AttachmentsDefinition.js
+++ Attachment/AttachmentsDefinition.js
@@ -351,6 +351,13 @@ export const ALL_ATTACHMENT_TYPES_JSON =
     '               "validation_argument":"TODAY"\n' +
     '            }\n' +
     '         ]\n' +
+    '      },\n' +
+    '      { \n' +
+    '         "country":"PL",\n' +
+    '         "type":"contracting",\n' +
+    '         "label":"mrc.attachments.types.contracting",\n' +
+    '         "remark":"Contract",\n' +
+    '         "is_collateral":false\n' +
     '      }\n' +
     '   ]\n' +
     '}';
