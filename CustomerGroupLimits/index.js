2223938c220e3781a417b0d89a1d83da53eaa456
Gheorghe Nastasache <gheorghe.nastasache@metrosystems.net>
2020-04-10 10:49:44 +0300

    MRC-2828, MRC-3997 - Approved limit should not appear in credit limit

--- CustomerGroupLimits/index.js
+++ CustomerGroupLimits/index.js
@@ -4,7 +4,6 @@ import './index.scss';
 import { lookup } from '../Util/translations';
 
 export default class CustomerGroupLimits extends Component {
-
     constructor(props) {
         super(props);
     }
@@ -15,77 +14,78 @@ export default class CustomerGroupLimits extends Component {
                 <h3 className="span-metro-blue">{lookup('mrc.label.customergroup')}</h3>
                 <table className="table-group-limit">
                     <tbody>
-                    <tr>
-                        <td className="td-left">
-                            <p>
-                                {lookup('mrc.label.availablegrouplimit')}: <span> </span>
-                            </p>
-                        </td>
-                        <td className="td-right">
-                            <mrc-number show-currency-for-country={this.props.country}>
-                                {this.props.availableGroupLimit}
-                            </mrc-number>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="td-left">
-                            <p>
-                                {lookup('mrc.label.exhaustionGroupLimit')}: <span> </span>
-                            </p>
-                        </td>
-                        <td className="td-right">
-                            <mrc-number show-currency-for-country={this.props.country}>
-                                {this.props.exhaustionGroupLimit}
-                            </mrc-number>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="td-left">
-                            <p>
-                                {lookup('mrc.label.currentgrouplimit')}: <span> </span>
-                            </p>
-                        </td>
-                        <td className="td-right">
-                            <mrc-number show-currency-for-country={this.props.country}>
-                                {this.props.currentGroupLimit}
-                            </mrc-number>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="td-left">
-                            <p>
-                                {lookup('mrc.label.requestedgrouplimit')}: <span> </span>
-                            </p>
-                        </td>
-                        <td className="td-right">
-                            <mrc-number dynamic={this.props.requestedGroupLimit}
-                                        show-currency-for-country={this.props.country}>
-                                {this.props.requestedGroupLimit}
-                            </mrc-number>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="td-left">
-                            <p>
-                                {lookup('mrc.label.approvedgrouplimit')}: <span> </span>
-                            </p>
-                        </td>
-                        <td className="td-right">
-                            <mrc-number
-                                dynamic={this.props.approvedGroupLimitInst}
-                                show-currency-for-country={this.props.country}
-                            >
-                                {this.props.approvedGroupLimitInst}
-                            </mrc-number>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td className="td-left">
+                                <p>
+                                    {lookup('mrc.label.availablegrouplimit')}: <span> </span>
+                                </p>
+                            </td>
+                            <td className="td-right">
+                                <mrc-number show-currency-for-country={this.props.country}>
+                                    {this.props.availableGroupLimit}
+                                </mrc-number>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="td-left">
+                                <p>
+                                    {lookup('mrc.label.exhaustionGroupLimit')}: <span> </span>
+                                </p>
+                            </td>
+                            <td className="td-right">
+                                <mrc-number show-currency-for-country={this.props.country}>
+                                    {this.props.exhaustionGroupLimit}
+                                </mrc-number>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="td-left">
+                                <p>
+                                    {lookup('mrc.label.currentgrouplimit')}: <span> </span>
+                                </p>
+                            </td>
+                            <td className="td-right">
+                                <mrc-number show-currency-for-country={this.props.country}>
+                                    {this.props.currentGroupLimit}
+                                </mrc-number>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="td-left">
+                                <p>
+                                    {lookup('mrc.label.requestedgrouplimit')}: <span> </span>
+                                </p>
+                            </td>
+                            <td className="td-right">
+                                <mrc-number
+                                    dynamic={this.props.requestedGroupLimit}
+                                    show-currency-for-country={this.props.country}
+                                >
+                                    {this.props.requestedGroupLimit}
+                                </mrc-number>
+                            </td>
+                        </tr>
+                        {this.props.hideApprovedGroupLimit ? null : (
+                            <tr>
+                                <td className="td-left">
+                                    <p>
+                                        {lookup('mrc.label.approvedgrouplimit')}: <span> </span>
+                                    </p>
+                                </td>
+                                <td className="td-right">
+                                    <mrc-number
+                                        dynamic={this.props.approvedGroupLimitInst}
+                                        show-currency-for-country={this.props.country}
+                                    >
+                                        {this.props.approvedGroupLimitInst}
+                                    </mrc-number>
+                                </td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
-
-
         );
-
     }
 }
 
@@ -95,7 +95,6 @@ CustomerGroupLimits.propTypes = {
     exhaustionGroupLimit: PropTypes.number,
     currentGroupLimit: PropTypes.number,
     requestedGroupLimit: PropTypes.number,
-    approvedGroupLimitInst: PropTypes.number
-
-
+    approvedGroupLimitInst: PropTypes.number,
+    hideApprovedGroupLimit: PropTypes.bool,
 };
