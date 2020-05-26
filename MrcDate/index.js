a2932ef5e4f6b5e8ecce2b475fa18661fc1be2a3
steffen.henschel01 <steffen.henschel01@metronom-external.com>
2020-05-26 13:50:46 +0200

    Date with 2 digit day and month

--- MrcDate/index.js
+++ MrcDate/index.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 export default class MrcDate extends PureComponent {
     render() {
         let { children: dateString } = this.props;
-        let localeDateString = new Date(dateString).toLocaleDateString();
+        let localeDateString = new Date(dateString).toLocaleDateString(undefined, {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+        });
         return (
             <time dateTime={dateString} className="mrc-ui-date">
                 {localeDateString}
