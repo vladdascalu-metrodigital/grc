1bbc9c0d54720f1d8577d484b47a9306ae2f2178
steffen.henschel01 <steffen.henschel01@metronom-external.com>
2020-06-03 18:59:42 +0200

    Grid story for cols prop

--- stories/Grid.stories.js
+++ stories/Grid.stories.js
@@ -3,12 +3,25 @@ import { storiesOf } from '@storybook/react';
 import Grid from '../Grid';
 import Card, { TYPE } from '../Card';
 
-storiesOf('Layouts/Grid', module).add('super automatic grid', () => (
-    <Grid>
-        <Card>very</Card>
-        <Card type={TYPE.WARNING}>automatic</Card>
-        <Card>layout</Card>
-        <Card>of</Card>
-        <Card>things</Card>
-    </Grid>
-));
+storiesOf('Layouts/Grid', module)
+    .add('super automatic grid', () => (
+        <Grid>
+            <Card>very</Card>
+            <Card type={TYPE.WARNING}>automatic</Card>
+            <Card>layout</Card>
+            <Card>of</Card>
+            <Card>things</Card>
+        </Grid>
+    ))
+    .add('define columns', () => (
+        <Grid cols="4">
+            <Card>1</Card>
+            <Card>2</Card>
+            <Card>3</Card>
+            <Card>4</Card>
+            <Card>1</Card>
+            <Card>2</Card>
+            <Card>3</Card>
+            <Card>4</Card>
+        </Grid>
+    ));
