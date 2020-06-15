7deac631352bcb52e20d99aa4204244ab7b7dfd0
steffen.henschel01 <steffen.henschel01@metronom-external.com>
2020-06-15 11:38:30 +0200

    MainContent story, MrcTable story update

--- stories/MrcTable.stories.js
+++ stories/MrcTable.stories.js
@@ -5,25 +5,30 @@ import Table from '../MrcTable';
 storiesOf('layouts/Tables/MrcTable', module).add('sticky usage', () => (
     <Table.Root>
         <Table.Head>
-            <Table.R isSticky>
+            <Table.R sticky="sticky-group-id">
                 <Table.H>a</Table.H>
                 <Table.H>b</Table.H>
                 <Table.H>c</Table.H>
             </Table.R>
-            <Table.R isSticky>
+            <Table.R sticky="sticky-group-id">
                 <Table.H>
                     a<br />a
                 </Table.H>
                 <Table.H>b</Table.H>
                 <Table.H>c</Table.H>
             </Table.R>
-            <Table.R isSticky>
+            <Table.R sticky="sticky-group-id">
                 <Table.H>a</Table.H>
                 <Table.H>b</Table.H>
                 <Table.H>c</Table.H>
             </Table.R>
         </Table.Head>
         <Table.Body>
+            <Table.R key={'first'}>
+                <Table.D>1 first row</Table.D>
+                <Table.D>2 first row</Table.D>
+                <Table.D>3 first row</Table.D>
+            </Table.R>
             {[...Array(50).keys()].map((e, i) => (
                 <Table.R key={i}>
                     <Table.D>1</Table.D>
