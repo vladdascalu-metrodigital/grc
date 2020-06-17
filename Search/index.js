8303ecc51921ce21a57d2302ec79ff4f1efb68cd
steffen.henschel01 <steffen.henschel01@metronom-external.com>
2020-06-17 16:17:03 +0200

    Search enhancements

--- Search/index.js
+++ Search/index.js
@@ -7,27 +7,54 @@ import SearchIcon from '../icons/SearchIcon';
 import './index.scss';
 
 export default class Search extends Component {
-    handleChange(e) {
+    constructor(props) {
+        super(props);
+        this.inputRef = React.createRef();
+        this.state = {
+            showClearButton: false,
+        };
+    }
+
+    handleChange() {
         let { onChange } = this.props;
-        if (onChange) onChange(e.target.value);
+        if (onChange) onChange(this.inputRef.current.value);
+        this.setState({
+            showClearButton: !!this.inputRef.current.value,
+        });
     }
 
     handleEnterSearch(e) {
         let { onEnterSearch } = this.props;
-        if (e.keyCode == 13) onEnterSearch(e.target.value);
+        if (e.keyCode == 13 && onEnterSearch) onEnterSearch(this.inputRef.current.value);
+    }
+
+    handleClear() {
+        let { onEnterSearch } = this.props;
+        this.inputRef.current.value = '';
+        if (onEnterSearch) onEnterSearch(this.inputRef.current.value);
+        this.setState({
+            showClearButton: false,
+        });
     }
 
     render() {
         let { placeholder } = this.props;
+        let { showClearButton } = this.state;
         return (
             <div className="mrc-ui-search">
                 <input
+                    ref={this.inputRef}
                     onKeyUp={this.handleEnterSearch.bind(this)}
                     onChange={this.handleChange.bind(this)}
                     className="mrc-ui-search-input"
                     type="text"
                     placeholder={placeholder}
                 />
+                {showClearButton && (
+                    <div className="mrc-ui-search-clear">
+                        <a onClick={this.handleClear.bind(this)}>clear</a>
+                    </div>
+                )}
                 <div className="mrc-ui-search-icon">
                     <SearchIcon size={SIZE.XSMALL} fill={COLOR.NEUTRAL} />
                 </div>
