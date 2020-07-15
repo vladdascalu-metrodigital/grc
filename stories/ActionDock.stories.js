ac49ba6dde72708ccfa22ddfdbea24435debcb17
Steffen <mail@steffenhenschel.com>
2020-07-15 17:15:17 +0200

    move ActionDock story to Fundamentals stories

--- stories/ActionDock.stories.js
+++ stories/ActionDock.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import ActionDock, { SimpleActionDock } from '../ActionDock';
 import { action } from '@storybook/addon-actions';
 
-storiesOf('ActionDock/simple', module).add('Simple ActionDock', () => (
+storiesOf('Fundamentals/ActionDock', module).add('Simple ActionDock', () => (
     <SimpleActionDock onCancel={() => action('actionDockCancel')()} onApply={() => action('actionDockApply')()} />
 ));
