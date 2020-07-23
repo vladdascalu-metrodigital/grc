08dfd12b8833c63495bdbc8ac18c029347b98310
Steffen <mail@steffenhenschel.com>
2020-07-23 15:55:28 +0200

    jest: support for webpack static imports

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
+        '\\.(scss|css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transformIgnorePatterns: [
         'node_modules/(?!(react-native' +
