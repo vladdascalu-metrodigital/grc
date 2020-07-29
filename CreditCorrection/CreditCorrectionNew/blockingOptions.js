febc998119d674a604bf7469afffdd34a97d6c1d
Fei Qin <frei.qin@metronom.com>
2020-07-29 15:44:39 +0200

    Added new blocking options for HR and RS

--- CreditCorrection/CreditCorrectionNew/blockingOptions.js
+++ CreditCorrection/CreditCorrectionNew/blockingOptions.js
@@ -46,15 +46,28 @@ export const countryBlockingOptions = {
     ],
     RS: [
         {
-            id: 'GENERALBLOCK',
+            id: 'HARDBLOCK',
             translationKey: 'mrc.blocking-option.generalblock',
             label: 'mrc.blocking-option.generalblock',
         },
         {
-            id: 'SOFTBLOCK',
-            translationKey: 'mrc.blocking-option.softblock',
-            label: 'mrc.blocking-option.softblock',
+            id: 'CREDITTOCASH',
+            translationKey: 'mrc.blocking-option.credittocash',
+            label: 'mrc.blocking-option.credittocash',
             customerLevel: true,
+        },
+        {
+            id: 'REMOVEBLOCK',
+            translationKey: 'mrc.blocking-option.removeblock',
+            label: 'mrc.blocking-option.removeblock',
+            customerLevel: true,
+        },
+    ],
+    HR: [
+        {
+            id: 'HARDBLOCK',
+            translationKey: 'mrc.blocking-option.generalblock',
+            label: 'mrc.blocking-option.generalblock',
         },
         {
             id: 'CREDITTOCASH',
