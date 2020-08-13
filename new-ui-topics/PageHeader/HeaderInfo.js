02eb4d23322d9f9dae2a78382d6a5bd644918fac
Steffen <mail@steffenhenschel.com>
2020-08-13 15:18:59 +0200

    MRC-4243: HeaderInfoRequestSummary styling

--- new-ui-topics/PageHeader/HeaderInfo.js
+++ new-ui-topics/PageHeader/HeaderInfo.js
@@ -7,7 +7,10 @@ import PropTypes from 'prop-types';
 // import RocketIcon from '../../icons/RocketIcon';
 
 import HeaderInfoRequestStart, { headerInfoRequestStartDataPropType } from './HeaderInfoRequestStart';
-import HeaderInfoRequestSummary, { HeaderInfoRequestSummaryDataPropType } from './HeaderInfoRequestSummary';
+import HeaderInfoRequestSummary, {
+    HeaderInfoRequestSummaryDataPropType,
+    STATUS as RSTATUS,
+} from './HeaderInfoRequestSummary';
 
 import './HeaderInfo.scss';
 
@@ -17,7 +20,13 @@ export default class HeaderInfos extends PureComponent {
         return (
             <div className={'mrc-ui-header-info'}>
                 <HeaderInfoRequestStart requestStartDate={requestStartDate} isColStyle={isColStyle} />
-                <HeaderInfoRequestSummary />
+                <HeaderInfoRequestSummary
+                    phaseName="Initialisierung"
+                    phaseNumber="1"
+                    phaseNumberMax="5"
+                    status={RSTATUS.PENDING}
+                    statusRole="HOT"
+                />
             </div>
         );
     }
--- new-ui-topics/PageHeader/HeaderInfoRequestSummary.js
+++ new-ui-topics/PageHeader/HeaderInfoRequestSummary.js
@@ -8,21 +8,33 @@ import classnames from 'classnames';
 
 import './HeaderInfoRequestSummary.scss';
 
+export const STATUS = {
+    PENDING: 'PENDING',
+    // TODO: add all available status keys
+};
+
 export default class HeaderInfoRequestSummary extends PureComponent {
     render() {
-        let { isColStyle } = this.props;
+        let { isColStyle, phaseName, phaseNumber, phaseNumberMax, status, statusRole } = this.props;
         let className = classnames('mrc-ui-hi-rsummary', {
             'mrc-ui-hi-rsummary-colstyle': isColStyle,
         });
+        let statusValueClassName = classnames('mrc-ui-hi-rsummary-status-value', {
+            'mrc-ui-hi-rsummary-status-value-green': status === STATUS.PENDING,
+        });
         return (
             <div className={className}>
-                <div className="mrc-ui-hi-rsummary-phase-label">Phase</div>
-                <div className="mrc-ui-hi-rsummary-phase">Initialisierung 1/5</div>
-                <div className="mrc-ui-hi-rsummary-status-label">Status</div>
-                <div className="mrc-ui-hi-rsummary-status">
-                    <span className="mrc-ui-hi-rsummary-status-value">PENDING</span>
-                    by
-                    <span className="mrc-ui-hi-rsummary-status-role">HOT</span>
+                <div className="mrc-ui-hi-rsummary-row">
+                    <div className="mrc-ui-hi-rsummary-phase-label">Phase</div>
+                    <div className="mrc-ui-hi-rsummary-phase">{`${phaseName} ${phaseNumber}/${phaseNumberMax}`}</div>
+                </div>
+                <div className="mrc-ui-hi-rsummary-row">
+                    <div className="mrc-ui-hi-rsummary-status-label">Status</div>
+                    <div className="mrc-ui-hi-rsummary-status">
+                        <span className={statusValueClassName}>{status}</span>
+                        by
+                        <span className="mrc-ui-hi-rsummary-status-role">{statusRole}</span>
+                    </div>
                 </div>
             </div>
         );
@@ -32,7 +44,8 @@ export default class HeaderInfoRequestSummary extends PureComponent {
 export const HeaderInfoRequestSummaryDataPropType = {
     phaseName: PropTypes.string,
     phaseNumber: PropTypes.number,
-    status: PropTypes.oneOf(['PENDING']), // TODO: add all available status keys
+    phaseNumberMax: PropTypes.number,
+    status: PropTypes.oneOf(Object.values(STATUS)),
     statusRole: PropTypes.oneOf(['CC, CM, HOT']), // TODO: add all concerning roles
 };
 
