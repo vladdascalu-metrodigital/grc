5aa963237721f7e441cb361efdd848271dd6c735
Ludvig Sundstrom <ludvig.sundstroem@innoq.com>
2020-09-14 10:37:18 +0200

    MRC-4273: Add credit correction options for RO

--- CreditCorrection/CreditCorrection/blockingOptions.js
+++ CreditCorrection/CreditCorrection/blockingOptions.js
@@ -44,6 +44,26 @@ export const countryBlockingOptions = {
             customerLevel: true,
         },
     ],
+    RO: [
+        {
+            id: 'SOFTBLOCK',
+            translationKey: 'mrc.blocking-option.softblock',
+            label: 'mrc.blocking-option.softblock',
+            customerLevel: true,
+        },
+        {
+            id: 'CREDITTOCASH',
+            translationKey: 'mrc.blocking-option.credittocash',
+            label: 'mrc.blocking-option.credittocash',
+            customerLevel: true,
+        },
+        {
+            id: 'REMOVEBLOCK',
+            translationKey: 'mrc.blocking-option.removeblock',
+            label: 'mrc.blocking-option.removeblock',
+            customerLevel: true,
+        },
+    ],
     RS: [
         {
             id: 'HARDBLOCK',
